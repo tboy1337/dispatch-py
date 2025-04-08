b41c785206b931b9fe888784c78345200fe7b0b8
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-04-08 17:37:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="dispatch-proxy",
+    name="dispatch-proxy-py",
     version="0.1.0",
     description="A SOCKS proxy that balances traffic between network interfaces",
     author="Dispatch Team",
