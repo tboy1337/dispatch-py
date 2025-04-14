c2139c34742fbbad4dc7428529222dde8b6e7323
tboy1337 <obywhuie@anonaddy.com>
2025-04-14 14:48:00 +0700

    Enhance setup.py with long description, author details, and project URLs

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name="dispatch-py",
     version="0.1.0",
     description="A SOCKS proxy that balances traffic between network interfaces",
-    author="Dispatch Team",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tboy1337/dispatch-py",
+    download_url="https://github.com/tboy1337/dispatch-py/releases/latest",
+    author="tboy1337",
+    author_email="obywhuie@anonaddy.com",
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
